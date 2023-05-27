82b80102ed68ca4fed75d6c3f8047bdbef9c0fc0
olegario ballester <olegario.ballester@gmail.com>
2023-05-27 17:42:56 +0200

    Añadido diccionario fav_pizzas

--- helloworld.py
+++ helloworld.py
@@ -1,8 +1,19 @@
 import os
 os.system('clear')
 
-first_name = 'Olegario'
+'''first_name = 'Olegario'
 last_name = 'Ballester'
 
 print(f'Hello World, {first_name}')
-print(type(first_name))
+print(type(first_name))'''
+
+
+'''Diccionarios'''
+
+fav_pizzas = {
+	'John':'Peperoni',
+	'Tim' : 'Mushrooms',
+	'Mary' : 'Cheese'
+}
+for key, value in fav_pizzas.items():
+	print(f'{key} --> {value}.')

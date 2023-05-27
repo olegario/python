670e2ce8d6c5ae1dd0dc9d4331575824f8015005
olegario ballester <olegario.ballester@gmail.com>
2023-05-27 18:10:46 +0200

    Añadido Fizzbuzz

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,7 @@
+############################
+# Autor: Olegario Ballester
+############################
+
 import os
 os.system('clear')
 
@@ -10,10 +14,39 @@ print(type(first_name))'''
 
 '''Diccionarios'''
 
-fav_pizzas = {
+'''fav_pizzas = {
 	'John':'Peperoni',
 	'Tim' : 'Mushrooms',
 	'Mary' : 'Cheese'
 }
 for key, value in fav_pizzas.items():
 	print(f'{key} --> {value}.')
+'''
+''' Fizzbuzz
+	%3 Fizz
+	%5 buzz
+	%3 & %5 Fizzbuzz'''
+
+count_3 = 0
+count_5 = 0
+count_3_5 = 0
+count = 0
+
+
+for i in range(1,101):
+	if i%3 == 0 and i%5==0:
+		count_3_5 += 1
+		print(f'{i} Fizzbuzz')
+	elif i%5 == 0:
+		count_5 += 1
+		print(f'{i} buzz')
+	elif i%3==0 :
+		count_3 += 1
+		print(f'{i} Fizz')
+	else:
+		count += 1
+		print(f'{i}')
+
+print(f' Fizz: {count_3}\nbuzz: {count_5}\nFizzbuzz: {count_3_5}\nResto:{count} ')
+		
+		
